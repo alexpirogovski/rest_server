8d34480fde6377b47dd74004d4ed39e55c131ff9
alexpirogovski <pirogovski@gmail.com>
2017-04-18 15:41:03 +0300

    Added PhiloJSONEncoder class that jsonifies Philosophers class and assigned it as JSONEncoder for app

---
+++ philoJSONEncoder_class.py
@@ -0,0 +1,12 @@
+from flask.json import JSONEncoder
+
+from philosopher import Philosopher
+
+
+class PhiloJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Philosopher):
+            return {    'name'     : obj.name   }
+
+
+        return super(PhiloJSONEncoder, self).default(obj)
